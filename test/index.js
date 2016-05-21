da9521e522fdfea786fdf0f7a0b7d394554b4003
Olivier Lalonde <olalonde@gmail.com>
2016-05-21 00:38:32 -0700

    one more test, with promise

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ import test from 'blue-tape'
 import initMemoize from '../src'
 import initBlobStore from 'fs-blob-store'
 import rimraf from 'rimraf'
+import fs from 'fs'
 import { join } from 'path'
 
 const tmpPath = join(__dirname, '.tmp')
@@ -27,6 +28,44 @@ test('simple cache miss, cache hit', (t) => {
     .then(() => square(2))
     .then((val) => {
       t.equal(val, 4, 'returns cached result')
+    })
+    .then(() => {
+      t.end()
+    })
+    .catch((err) => {
+      t.fail(err)
+    })
+})
+
+// promisified read file
+// returns buffer
+const readFile = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) reject(err)
+    resolve(data)
+  })
+})
+
+const poemPath = `${__dirname}/poem.txt`
+const poemData = fs.readFileSync(poemPath).toString()
+
+test('simple cache miss, cache hit with promise', (t) => {
+  let callCount = 0
+  const readPoem = memoize(() => {
+    callCount++
+    if (callCount === 1) {
+      return readFile(poemPath).then((data) => data.toString())
+    }
+    return Promise.resolve('oopsie')
+  }, 'test-1')
+
+  readPoem()
+    .then((val) => {
+      t.equal(val, poemData, 'poem data')
+    })
+    .then(() => readPoem())
+    .then((val) => {
+      t.equal(val, poemData, 'returns cached result')
     })
     .then(() => {
       t.end()
